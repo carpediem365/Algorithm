5938d31444528f4789aa88454d16f94849573afd
carpediem365 <136786274+carpediem365@users.noreply.github.com>
2023-07-04 19:58:06 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,17 @@
+#9번씩 입력받고 리스트에 저장해보자
+all_list = []
+
+max_num =-1
+col_index =0
+row_index = 0
+for i in range(9):
+    num = list(map(int,input().split()))
+    all_list.append(num)
+    if max(num) > max_num:
+        max_num = max(num)
+        row_index = i+1
+        col_index = num.index(max_num)+1
+   
+#print(all_list)        
+print(max_num) 
+print(row_index,col_index)    
