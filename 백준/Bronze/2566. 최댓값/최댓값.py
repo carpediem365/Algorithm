6cc846a7f1c15bd799e1c7f0f62c625878fe08b0
carpediem365 <136786274+carpediem365@users.noreply.github.com>
2023-07-04 20:01:23 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,13 +1,13 @@
 #9번씩 입력받고 리스트에 저장해보자
 all_list = []
 
-max_num =-1
+max_num =0
 col_index =0
 row_index = 0
 for i in range(9):
     num = list(map(int,input().split()))
     all_list.append(num)
-    if max(num) > max_num:
+    if max(num) >=max_num:
         max_num = max(num)
         row_index = i+1
         col_index = num.index(max_num)+1
